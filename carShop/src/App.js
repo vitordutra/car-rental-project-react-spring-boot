bc7d133e876149018c92fabe8d0942d8673c1f5f
guilhermeSantos <guilhermeigatu99@gmail.com>
2022-05-18 22:27:25 -0300

    Arquivos iniciais

---
+++ carShop/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Routes from './routes/Routes';
+import Footer from './component/Footer/Footer';
+import Header from './component/Header/Header';
+
+const App = () => {
+    
+  
+  return (
+    <>
+        <Header />
+        <Routes />
+        <Footer />
+    </>    
+  )
+};
+
+export default App;
