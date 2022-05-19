214cee3ed8bb0d6118223af34a7b9d0ff3f9982c
guilhermeSantos <guilhermeigatu99@gmail.com>
2022-05-19 14:12:42 -0300

    alterando una coisa

--- carShop/src/pages/Main/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Main(){
-    return(
-        <h1>Página inicial</h1>
-    );
-};
-
-export default Main;
--- carShop/src/pages/SobreEmpresa/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SobreEmpresa(){
-    return(
-        <h1>Página Sobre Empresa</h1>
-    );
-};
-
-export default SobreEmpresa;
