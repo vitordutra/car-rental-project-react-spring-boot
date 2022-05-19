442c75192ddb0403b87ec35481e49cda15a78d00
guilhermeSantos <guilhermeigatu99@gmail.com>
2022-05-18 22:31:00 -0300

    Header e footer, favicon

---
+++ carShop/src/component/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Footer = (props) => {
+  return (
+   <>
+   <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2022 GuilhermeSik. Design by{' '}
+            <a href='https://www.linkedin.com/in/guilherme-santos-38b562206/' rel='nofollow'>
+              Guilherme-Sik
+            </a>
+          </p>
+        </div>
+      </div>
+   
+   </>
+  );
+}
+
+export default Footer;
