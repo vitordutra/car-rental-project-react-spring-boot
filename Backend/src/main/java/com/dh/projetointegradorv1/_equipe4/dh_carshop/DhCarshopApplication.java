2f7db22e81b606d80655033102e8b3e2aec84fc8
Fran <francieliceleghim2013@gmail.com>
2022-05-23 20:22:37 -0300

    configuração spring e backend em pasta propria

---
+++ Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/DhCarshopApplication.java
@@ -0,0 +1,17 @@
+package com.dh.projetointegradorv1._equipe4.dh_carshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DhCarshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhCarshopApplication.class, args);
+	}
+
+
+}
