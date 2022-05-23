0e8b414cd575a44001000db2f78e8d4e6be86c20
Fran <francieliceleghim2013@gmail.com>
2022-05-23 20:29:44 -0300

    Atualizando .gitignore

--- Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/DhCarshopApplication.java
+++ Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/DhCarshopApplication.java
@@ -2,7 +2,6 @@ package com.dh.projetointegradorv1._equipe4.dh_carshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
