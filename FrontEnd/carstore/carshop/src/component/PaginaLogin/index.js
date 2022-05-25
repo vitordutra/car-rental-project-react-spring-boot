f79edf2c56708832c85cfe038cde6d495cb7a5bd
rows <marceloxpaduan@gmail.com>
2022-05-24 23:18:16 -0300

    Funcionando

--- FrontEnd/carstore/carshop/src/component/PaginaLogin/index.js
+++ FrontEnd/carstore/carshop/src/component/PaginaLogin/index.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 
 import { AuthContext } from "../../../src/context/auth";
 
-import './index.css';
+import './styles.css';
 
 const PaginaLogin = () => {
     const { authenticated, login } = useContext(AuthContext);
