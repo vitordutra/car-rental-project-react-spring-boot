0ab27de2deb4a0c25b1764bc9ef3b323fccd43cb
rows <marceloxpaduan@gmail.com>
2022-05-25 00:10:00 -0300

    primeiracomit

---
+++ FrontEnd/carstore/carshop/src/component/Home/Carousel/index.js
@@ -0,0 +1,18 @@
+const carrousel = (props) => {
+    return (
+     <>
+      
+  
+          <div className='container text-center'>
+            <p>
+              Aqui é a homepage, maluco
+              
+            </p>
+          </div>
+
+     
+     </>
+    );
+  }
+  
+  export default carrousel; 
--- FrontEnd/carstore/carshop/src/component/Home/index.js
+++ FrontEnd/carstore/carshop/src/component/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './styles.css';
+import carrousel from "./Carousel";
 
 
 const home = (props) => {
