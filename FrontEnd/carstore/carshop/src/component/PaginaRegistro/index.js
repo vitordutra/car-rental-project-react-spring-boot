bf488cf653ed0fc04787e60d639f6b1528129679
rows <marceloxpaduan@gmail.com>
2022-05-27 18:46:22 -0300

    Juntado branch marcelo + lia sobre a main

---
+++ FrontEnd/carstore/carshop/src/component/PaginaRegistro/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import './styles.css';
+
+const PaginaRegistro =() => {
+    return (
+        <>
+        <div class="container" >
+    <a class="links" id="paracadastro"></a>
+    <a class="links" id="paralogin"></a>
+    
+    <div class="content">      
+      
+        
+     
+      <div id="cadastro">
+        <form method="post" action=""> 
+          <h1>Cadastro</h1> 
+          
+          <p> 
+            <label for="nome_cad">Seu nome</label>
+            <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder=" Maria " />
+          </p>
+          
+          <p> 
+            <label for="email_cad">Seu e-mail</label>
+            <input id="email_cad" name="email_cad" required="required" type="email" placeholder="modeloteste001@gmail.com"/> 
+          </p>
+          
+          <p> 
+            <label for="senha_cad">Sua senha</label>
+            <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="1234"/>
+          </p>
+          
+          <p> 
+            <input type="submit" value="Cadastrar"/> 
+          </p>
+          
+          <p class="link">  
+            Já tem conta?
+            <a href="/login"> Ir para Login </a>
+            
+          </p>
+        </form>
+      </div>
+    </div>
+  </div> 
+
+
+
+
+        </>
+    )
+}
+
+export default PaginaRegistro; 
