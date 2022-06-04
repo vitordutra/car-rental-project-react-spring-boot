db61333bc29152a6e2c91921531502d8ff03a7d6
Vitor Dutra <jo.vitor.d@gmail.com>
2022-06-04 17:23:46 -0300

    Create Feature & Add Many to Many relationship with Product

--- Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/model/Feature.java
+++ Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/model/Feature.java
@@ -1,2 +1,76 @@
-package com.dh.projetointegradorv1._equipe4.dh_carshop.model;public class Feature {
+package com.dh.projetointegradorv1._equipe4.dh_carshop.model;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "features")
+public class Feature implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String icone;
+
+    @ManyToMany(mappedBy = "caracteristicas")
+    private Set<Product> produtos;
+
+    // Timestamps automáticos
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private OffsetDateTime criado;
+    @Column(columnDefinition = "TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private OffsetDateTime atualizado;
+
+    @PrePersist
+    public void antesDeSalvar() {
+        criado = OffsetDateTime.now();
+    }
+    @PreUpdate
+    public void antesDeAtualizar() {
+        atualizado = OffsetDateTime.now();
+    }
+
+    public Feature() {
+    }
+
+    public Feature(String nome, String icone, OffsetDateTime criado, OffsetDateTime atualizado) {
+        this.nome = nome;
+        this.icone = icone;
+        this.criado = criado;
+        this.atualizado = atualizado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIcone() {
+        return icone;
+    }
+
+    public void setIcone(String icone) {
+        this.icone = icone;
+    }
+
+    @Override
+    public String toString() {
+        return "Feature{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", icone='" + icone + '\'' +
+                ", criado=" + criado +
+                ", atualizado=" + atualizado +
+                '}';
+    }
 }
