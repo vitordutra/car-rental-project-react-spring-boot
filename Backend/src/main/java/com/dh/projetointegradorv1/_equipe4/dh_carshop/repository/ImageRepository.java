94d72315635c27d234192b1d47f25893bd341f7e
Vitor Dutra <jo.vitor.d@gmail.com>
2022-06-04 18:02:28 -0300

    Add Repositories

--- Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/repository/ImageRepository.java
+++ Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/repository/ImageRepository.java
@@ -1,9 +1,9 @@
 package com.dh.projetointegradorv1._equipe4.dh_carshop.repository;
 
-import com.dh.projetointegradorv1._equipe4.dh_carshop.model.Category;
+import com.dh.projetointegradorv1._equipe4.dh_carshop.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageRepository extends JpaRepository<Category, Integer> {
+public interface ImageRepository extends JpaRepository<Image, Integer> {
 }
--- Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/repository/ProductRepository.java
+++ Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/repository/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.dh.projetointegradorv1._equipe4.dh_carshop.repository;
 
-import com.dh.projetointegradorv1._equipe4.dh_carshop.model.Category;
+import com.dh.projetointegradorv1._equipe4.dh_carshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Category, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
