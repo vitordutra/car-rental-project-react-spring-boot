bc16d6c145c0b89346a48dc7c69d78ec5c11bc4a
rows <marceloxpaduan@gmail.com>
2022-06-19 02:14:53 -0300

    pagina feita, falta integrar então não vou fechar a issue ainda

---
+++ FrontEnd/carstore/carshop/src/pages/ReservaBemSucedida/index.js
@@ -0,0 +1,21 @@
+import './styles.css';
+
+
+export default function ProductsCategory() {
+
+    return(
+<div className="container-reserva-sucedida">
+<h2>Reserva bem sucedida!</h2>
+<img src="https://w7.pngwing.com/pngs/944/793/png-transparent-computer-icons-check-mark-presentation-symbol-check-list-miscellaneous-angle-text.png" alt="Reserva bem sucedida"  />
+<Link to='/'> <p> voltar para home</p></Link>
+
+
+
+
+
+
+
+
+</div>
+)
+}
