806c28f2ad115800b9a10d462a60724c999a8d9e
Mateus Benites Dias <mateusdias@outlook.com>
2022-06-27 14:22:51 +0000

    2022-06-27 - ProductRepository - Filtro por datas

--- Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/repository/ProductRepository.java
+++ Backend/src/main/java/com/dh/projetointegradorv1/_equipe4/dh_carshop/repository/ProductRepository.java
@@ -12,6 +12,14 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(value = "SELECT * FROM products p WHERE p.id_cidade = ?1", nativeQuery = true)
     List<Product> findByCidade(Integer id);
 
+    /*@Query(value = "SELECT * FROM products p " +
+            "INNER JOIN bookings b ON ((p.id <> b.id_produto) OR (p.id = b.id_produto " +
+            "WHERE (CONVERT(?1, DATE) < b.inicio_reserva AND CONVERT(?2, DATE) < b.inicio_reserva) OR " +
+            "(CONVERT(?1, DATE) > b.fim_reserva AND CONVERT(?2, DATE) > b.fim_reserva)))",
+            nativeQuery = true)
+    List<Product> findByDatas(String dataInicio, String dataFim);*/
+
+
     /*@Query(value = "SELECT * FROM products p" +
             "INNER JOIN categories_products cp ON p.id_produto = cp.id_produto" +
             "WHERE cp.id_categoria = ?1", nativeQuery = true)
