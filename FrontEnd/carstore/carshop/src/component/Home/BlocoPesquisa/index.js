a5c136e239427a9602ae64baf8e9d57b6b075485
Vitor Dutra <jo.vitor.d@gmail.com>
2022-07-07 16:58:17 -0300

    Bloco de Pesquisa Funcional

--- FrontEnd/carstore/carshop/src/component/Home/BlocoPesquisa/index.js
+++ FrontEnd/carstore/carshop/src/component/Home/BlocoPesquisa/index.js
@@ -1,85 +1,82 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react';
 import './styles.css';
 import format from 'date-fns/format';
-import { Link } from 'react-router-dom'
-import api from "../../../services/api";
+import { Link } from 'react-router-dom';
+import api from '../../../services/api';
 
 import { DateRange } from 'react-date-range';
 
-import { addDays, set } from 'date-fns'
+import { addDays, set } from 'date-fns';
 
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 import { useNavigate } from 'react-router';
 
-const BlocoPesquisa = ({handleFilter}) => {
+const BlocoPesquisa = ({ handleFilter }) => {
+  const [cidade, setCidade] = useState('');
+  const [cidades, setCidades] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [stringTexto, setStringTexto] = useState('');
+  const [openSugestions, setOpenSugestions] = useState(false);
 
-    const [cidade, setCidade] = useState("");
-    const [cidades, setCidades] = useState([]);
-    const [inputValue,setInputValue] = useState("");
-    const [stringTexto,setStringTexto] = useState("");
-    const [openSugestions,setOpenSugestions] = useState(false)
-
-    const onChange = (event) =>{
-      setOpenSugestions(true);
-      handleShowSugestions(true);
-      setInputValue(event.target.value);
-      
+  const onChange = event => {
+    setOpenSugestions(true);
+    handleShowSugestions(true);
+    setInputValue(event.target.value);
+  };
+  async function callCidadesApi() {
+    try {
+      console.log('Entrou em BlocoPesquisa, Linha 29');
+      const response = await api.get('/api/v1/cities');
+      setCidades(response.data);
+    } catch (error) {
+      console.log(error);
     }
-    async function callCidadesApi() {
-      try {
-        const response = await api.get("/api/v1/cities");
-        setCidades(response.data);
-      }
-      catch (error) { 
-  
-      }
-    }
-    useEffect(()=>{
-      callCidadesApi()
+  }
 
-
-
-
-    },[])
-    
-    
-    
-    
-    
-
-     
-/*   useEffect(() => {
-    callCidadesApi(); 
+  useEffect(() => {
+    callCidadesApi();
   }, []);
-    
-    
+
+  /*   useEffect(() => {
+    callCidadesApi();
+  }, []);
+
+
   async function callCidadesApi() {
     try {
       const response = await api.get("/cities");
       setCidade(response.data);
     }
-    catch (error) { 
+    catch (error) {
 
     }
-    
-    } */
-    
-    async function callProductByCity(id) {
-        try {
-          const response = await api.get(`products?cityId=${id}`);
-          handleFilter(response.data);
-        }
-        catch (error) { 
-      
-        }
-      }
 
-    //Parte do calendário ###############################################################################################################
-    const refOne = useRef(null)
-    const minDate = new Date( new Date().getFullYear(), new Date().getMonth(),new Date().getDate());
-    const maxDate = new Date( new Date().getFullYear(), new Date().getMonth()+2,new Date().getDate());
-/* 
+    } */
+
+  async function callProductByCity(id) {
+    try {
+      console.log('Entrou em BlocoPesquisa, Linha 29');
+      const response = await api.get(`products/city/${id}`);
+      handleFilter(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Parte do calendário ###############################################################################################################
+  const refOne = useRef(null);
+  const minDate = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate()
+  );
+  const maxDate = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 2,
+    new Date().getDate()
+  );
+  /*
     async function callApiProductsDateRange(range, cidadeEscolhida) {
 
         try {
@@ -101,177 +98,161 @@ const BlocoPesquisa = ({handleFilter}) => {
         }
     } */
 
+  useEffect(() => {
+    // event listeners
+    document.addEventListener('keydown', hideOnEscape, true);
+  }, []);
 
-    useEffect(() => {
-      // event listeners
-      document.addEventListener("keydown", hideOnEscape, true)
-      
-    }, [])
-  
-    // hide dropdown on ESC press
-    const hideOnEscape = (e) => {
-      // console.log(e.key)
-      if( e.key === "Escape" ) {
-        setOpen(false)
-        setOpenSugestions(false)
-      }
+  // hide dropdown on ESC press
+  const hideOnEscape = e => {
+    // console.log(e.key)
+    if (e.key === 'Escape') {
+      setOpen(false);
+      setOpenSugestions(false);
     }
-  
+  };
 
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 1),
+      key: 'selection',
+    },
+  ]);
+  const [open, setOpen] = useState(false);
 
+  const navigate = useNavigate();
 
-
-    
-    const [range, setRange] = useState([
-      {
-        startDate:  new Date(),
-        endDate: addDays(new Date(), 1),
-        key: 'selection'
-      }
-    ])
-    const [open, setOpen] = useState(false)
-
-    const navigate  = useNavigate()
-
-    const handleSearch = () =>{
-      
-      /* navigate("/Produtos",{state: {range,cidade}}); */
-      navigate(`/Produtos/${cidade}/${format(range[0].startDate, "yyyy-MM-dd")}/${format(range[0].endDate, "yyyy-MM-dd")}`);
-
-
+  const handleSearch = () => {
+    /* navigate("/Produtos",{state: {range,cidade}}); */
+    if (cidade !== '') {
+      navigate(
+        `/Produtos/${cidade}/${format(
+          range[0].startDate,
+          'yyyy-MM-dd'
+        )}/${format(range[0].endDate, 'yyyy-MM-dd')}`
+      );
+    } else {
+      navigate(
+        `/Produtos/${format(range[0].startDate, 'yyyy-MM-dd')}/${format(
+          range[0].endDate,
+          'yyyy-MM-dd'
+        )}`
+      );
     }
+  };
 
-    const handleSelectCity= (value,text)=> {
-      
-      setStringTexto(text.toString());
-      
-      document.getElementById("pesquisa-retirar-local").value = stringTexto
-      setCidade(value)
-      
-      
+  const handleSelectCity = (value, text) => {
+    setStringTexto(text.toString());
 
+    document.getElementById('pesquisa-retirar-local').value = stringTexto;
+    setCidade(value);
+  };
+
+  const handleShowSugestions = () => {
+    const robson = document.getElementsByClassName('dropdown')[0];
+    console.log('robson: ', robson);
+    console.log('openSugestions: ', openSugestions);
+    console.log('cidade: ', cidade);
+
+    if (openSugestions) {
+      robson.style.display = 'none';
+      setOpenSugestions(!openSugestions);
+      setCidade(null);
+    } else {
+      robson.style.display = '';
+      setOpenSugestions(!openSugestions);
     }
+  };
 
-    const handleShowSugestions = () =>{
-      
-      const robson = document.getElementsByClassName("dropdown")[0];
-      
-      if (openSugestions){
-        robson.style.display = 'none';setOpenSugestions(!openSugestions);
-      } else {
-        robson.style.display = ''; setOpenSugestions(!openSugestions)}
-      
-    }
-
-
-        return (
-            <>  
-            <div>
-                <div className="pesquisa-primeira-fileira">
-                        <div className="pesquisa-itens-duplos">
-                        {/* <select className="select" name="cidade"  onChange={item => callProductByCity(item.target.value)}>
+  return (
+    <>
+      <div>
+        <div className='pesquisa-primeira-fileira'>
+          <div className='pesquisa-itens-duplos'>
+            {/* <select className="select" name="cidade"  onChange={item => callProductByCity(item.target.value)}>
               <option  value="">Escolha a cidade</option>
               {cidade.map((item) => (
               <option value={item.id}>{item.nome}</option>
               ))}
             </select> */}
 
+            <div className='search-container'>
+              <div className='search-inner'>
+                <input
+                  placeholder='Digite o local de retirada'
+                  type='text'
+                  id='pesquisa-retirar-local'
+                  name='pesquisa-retirar-local'
+                  className='pesquisa-inputs'
+                  value={inputValue}
+                  onChange={onChange}
+                  onClick={handleShowSugestions}
 
+                  /* value={inputValue} */
+                  /* onChange={item =>( setInputValue(item.target.text))} */
+                />
+              </div>
 
+              <div className='dropdown'>
+                {cidades
+                  .filter(item => {
+                    const searchTerm = inputValue.toString().toLowerCase();
+                    const fullName = item.nome.toString().toLowerCase();
 
-
-                          <div className="search-container">
-                            <div className="search-inner">
-                          <input
-                            placeholder="Digite o local de retirada"
-                            type="text"
-                            id="pesquisa-retirar-local"
-                            name="pesquisa-retirar-local"
-                            className="pesquisa-inputs"
-                            value={inputValue}
-                            onChange={onChange}
-                            onClick={handleShowSugestions}
-
-
-                            
-                            /* value={inputValue} */
-                            /* onChange={item =>( setInputValue(item.target.text))} */
-                            
-                            
-                            
-                            
-                           /> 
-                           </div>
-                    
-                      
-                      <div className="dropdown">
-                      {cidades.filter(item=>{
-                        const searchTerm = inputValue.toString().toLowerCase();
-                        const fullName = item.nome.toString().toLowerCase();
-
- 
-                        return fullName.includes(searchTerm) 
-
-                      })
-                      .map((item)=>
-                      <div  key={item.id} className="dropdown-row" ><option onClick={item =>handleSelectCity(item.target.value,item.target.text)} value={item.id} >{item.nome}, {item.estado}</option></div>
-                      )}
+                    return fullName.includes(searchTerm);
+                  })
+                  .map(item => (
+                    <div key={item.id} className='dropdown-row'>
+                      <option
+                        onClick={item =>
+                          handleSelectCity(item.target.value, item.target.text)
+                        }
+                        value={item.id}>
+                        {item.nome}, {item.estado}
+                      </option>
                     </div>
-                      </div>
-
-
-
-
-
-                    
-                    </div> 
-                    
-
-
-
-
-
-
-
-
-
-
-
-                    <div className="pesquisa-itens-duplos">
-                    <input
-                        value={`${format(range[0].startDate, "dd/MM/yyyy")} to ${format(range[0].endDate, "dd/MM/yyyy")}`}
-                        
-                        readOnly
-                        className="inputBox"
-                        onClick={ () => setOpen(open => !open) }
-                   />
-                    <div className="calendarFrontPage">
-
-                    
-                    {open && <DateRange 
-                        
-                        onChange={item => setRange([item.selection])}
-                        minDate={minDate}
-                        maxDate={maxDate} 
-                        editableDateInputs={true} 
-                        moveRangeOnFirstSelection={false}
-                        ranges={range}
-                        className="date"                            
-                        />}
-
-                    </div>
-                    </div>
-
-                    <input type="button" onClick={handleSearch} id="pesquisa-botao-buscar" name="pesquisa-botao-buscar" value="Buscar" /> 
-                </div>
+                  ))}
+              </div>
             </div>
-        </>
+          </div>
 
-    )
+          <div className='pesquisa-itens-duplos'>
+            <input
+              value={`${format(range[0].startDate, 'dd/MM/yyyy')} to ${format(
+                range[0].endDate,
+                'dd/MM/yyyy'
+              )}`}
+              readOnly
+              className='inputBox'
+              onClick={() => setOpen(open => !open)}
+            />
+            <div className='calendarFrontPage'>
+              {open && (
+                <DateRange
+                  onChange={item => setRange([item.selection])}
+                  minDate={minDate}
+                  maxDate={maxDate}
+                  editableDateInputs={true}
+                  moveRangeOnFirstSelection={false}
+                  ranges={range}
+                  className='date'
+                />
+              )}
+            </div>
+          </div>
 
-
-
-
-}
+          <input
+            type='button'
+            onClick={handleSearch}
+            id='pesquisa-botao-buscar'
+            name='pesquisa-botao-buscar'
+            value='Buscar'
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default BlocoPesquisa;
--- FrontEnd/carstore/carshop/src/pages/ProductsCategory/index.js
+++ FrontEnd/carstore/carshop/src/pages/ProductsCategory/index.js
@@ -1,191 +1,122 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import ProdCategory from "../../component/ProdCategory";
-import Products from "../Products";
-import api from "../../services/api";
+import { useEffect, useState } from 'react';
+import ProdCategory from '../../component/ProdCategory';
+import Products from '../Products';
+import api from '../../services/api';
 import { useLocation, useParams } from 'react-router-dom';
 import './styles.css';
 import format from 'date-fns/format';
 
-
 export default function ProductsCategory() {
-
-
-  
   const [products, setProducts] = useState([]);
   /* const location = useLocation(); */
   const params = useParams();
-  
+
   const [cidade, setCidade] = useState(params.cidade);
   const [startDate, setstartDate] = useState(params.startDate);
   const [endDate, setendDate] = useState(params.endDate);
- 
-
-/*   if (!(location.state === null)){
-    try{
-  setCidade(location.state.cidade)
-  setRange(location.state.range)
-}catch{
-
-} 
-} */
-
-
-
-   useEffect(() =>{ 
-    console.log("PRINTANDO LOCATION")
-    console.log(params)
-    console.log("before")
-    if (!(cidade === "") & !(startDate === undefined)) {
-      //console.log("1+2")
-      callApiProductsDateCity(cidade,startDate,endDate);
-      
-
-    } else{
-      if (!(cidade === undefined)) {
-      
-        //console.log("1")
-        callApiProductsCity(cidade);
-        
-      }else{
-        if (!(startDate === undefined)){
-          //console.log("2")
-          callApiProductsDate(startDate,endDate);
-        
-
-        }else{
-          //console.log("0")
-          callApiProducts();
-          
-        }
-     }
-
-    } 
-  
-  
-  
-  
-},[]);
-  
-
-  // Captura do id de categoria parâmetro passado para a página
-  /* const {categoryId} = useParams("categoryId");
 
   useEffect(() => {
-    if (categoryId === undefined) {
-      callApiProducts();
+    console.log('cidade: ', cidade);
+    console.log('startDate: ', startDate);
+    console.log('endDate: ', endDate);
+    if (!(cidade === '') & !(startDate === undefined)) {
+      //console.log("1+2")
+      callApiProductsDateCity(cidade, startDate, endDate);
     } else {
-      callApiProductsCategory();
-    
-    
+      if (!(cidade === undefined)) {
+        //console.log("1")
+        callApiProductsCity(cidade);
+      } else {
+        if (!(startDate === undefined)) {
+          //console.log("2")
+          callApiProductsDate(startDate, endDate);
+        } else {
+          //console.log("0")
+          callApiProducts();
+        }
+      }
     }
-    
-  }, []); */
+  }, []);
 
   async function callApiProductsCategory(categoryId) {
-
     try {
-      //const URL = "categories"
-    
-      //const URL = `products?categoryId=${categoryId}`
       const URL = `/category/${categoryId}`;
       const response = await api.get(URL);
       setProducts(response.data);
-    }
-    catch (error) {
-    }
+    } catch (error) {}
   }
 
   async function callApiProductsCity(cityId) {
-
     try {
-      
-    
-      const URL = `products?cityId=${cityId}`
+      const URL = `products/city/${cityId}`;
       const response = await api.get(URL);
       setProducts(response.data);
-    }
-    catch (error) {
-    }
+    } catch (error) {}
   }
-
-
-  
 
   async function callApiProducts() {
-
     try {
       //const URL = "categories"
-    
-      const URL = `/api/v1/products`
+
+      const URL = `/api/v1/products`;
       const response = await api.get(URL);
       setProducts(response.data);
-    }
-    catch (error) {
-    }
+    } catch (error) {}
   }
 
-  async function callApiProductsDateCity(cidade,startDate,endDate) {
-
+  async function callApiProductsDateCity(cidade, startDate, endDate) {
     try {
-        const DataDeInicio = startDate
-        const DataDeTermino = endDate
-        
-        console.log(DataDeInicio);
-        console.log(DataDeTermino);
-        const URL = `products?cityId=${cidade}`; /* &data_inicial=${DataDeInicio}&data_final=${DataDeTermino}`; */
-        // console.log(URL);
-        const response = await api.get(URL);
-        console.log(response);
-        setProducts(response.data);
+      console.log(
+        'Entrou em ProductsCategory, callApiProductsDateCity, Linha 72'
+      );
+      const DataDeInicio = startDate;
+      const DataDeTermino = endDate;
 
-
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
-
-async function callApiProductsDate(startDate,endDate) {
-
-  try {
-      const DataDeInicio = startDate
-      const DataDeTermino = endDate
-      
       console.log(DataDeInicio);
       console.log(DataDeTermino);
-      const URL = `products?data_inicial=${DataDeInicio}&data_final=${DataDeTermino}`;
-      // console.log(URL);
+      const URL = `api/v1/products?dataInicio=${DataDeInicio}&dataTermino=${DataDeTermino}`; /* &data_inicial=${DataDeInicio}&data_final=${DataDeTermino}`; */
+      console.log(URL);
       const response = await api.get(URL);
       console.log(response);
-      // handleFilter(response.data);
-
-
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  catch (error) {
-      console.log(error)
-  }
-}
- 
 
-  
+  async function callApiProductsDate(startDate, endDate) {
+    try {
+      console.log('Entrou em ProductsCategory, callApiProductsDate, Linha 92');
+      const DataDeInicio = startDate;
+      const DataDeTermino = endDate;
+
+      console.log(DataDeInicio);
+      console.log(DataDeTermino);
+      const URL = `api/v1/products?dataInicio=${DataDeInicio}&dataTermino=${DataDeTermino}`;
+      console.log(URL);
+      const response = await api.get(URL);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
-      <div className="ProCategory-body" >
-        <Products handleFilter={setProducts}/>
-      <h1 className="ProCategory-h1"></h1>
-      <ul className="ProCategory-ul">
-        <li className="ProCategory-li">
-            {products.map((item) => (
-          < ProdCategory key={item.id} prmProduct={item} />
-          
-        ))}
-            
+      <div className='ProCategory-body'>
+        <Products handleFilter={setProducts} />
+        <h1 className='ProCategory-h1'></h1>
+        <ul className='ProCategory-ul'>
+          <li className='ProCategory-li'>
+            {products.map(item => (
+              <ProdCategory key={item.id} prmProduct={item} />
+            ))}
           </li>
         </ul>
-        </div>
+      </div>
     </>
   );
-}  
+}
