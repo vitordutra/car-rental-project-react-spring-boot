3f7a6c53e3349c7ce5a6c33a376c9b70b5d83763
Fran <francieliceleghim2013@gmail.com>
2022-07-09 15:54:13 -0300

    responsividade: arrumado construtor CategoryDto, considerando imagem agora

--- FrontEnd/carstore/carshop/src/component/Home/Carousel/CategoryItem/index.js
+++ FrontEnd/carstore/carshop/src/component/Home/Carousel/CategoryItem/index.js
@@ -6,18 +6,16 @@ import { Link } from "react-router-dom";
 
 
 export default function CategoryItem({ prmCategory }) {
-
-
     return (
     <section className="categoryItemSection">   
         <ul className="categoryItemUl">
-                <li className="categoryItemLi">
-                    <Link className="categoryItemLink" to={`/api/v1/categories ${prmCategory.id}`}>
-                        <img className="categoryItemImg"  src={prmCategory.imagem} />
-                        <h3 className="categoryItemH3">{prmCategory.qualificacao}</h3>
-                        {<p className="categoryItemP" >{prmCategory.descricao}</p> }
-                    </Link>
-                </li>
+            <li className="categoryItemLi">
+                <Link className="categoryItemLink" to={`/api/v1/categories ${prmCategory.id}`}>
+                    <img className="categoryItemImg" src={prmCategory.url_imagem} />
+                    <h3 className="categoryItemH3">{prmCategory.qualificacao}</h3>
+                    {<p className="categoryItemP" >{prmCategory.descricao}</p> }
+                </Link>
+            </li>
         </ul>
     </section>         
     );
