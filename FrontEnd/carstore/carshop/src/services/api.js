4a06d553c655d1592acda5778691207e78f18cdc
Fran <francieliceleghim2013@gmail.com>
2022-07-15 10:52:11 -0300

    mudei a url do api front

--- FrontEnd/carstore/carshop/src/services/api.js
+++ FrontEnd/carstore/carshop/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   //baseURL: "https://my-json-server.typicode.com/rows15/db-projetointegrador/",
   
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://3.15.133.120/",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
